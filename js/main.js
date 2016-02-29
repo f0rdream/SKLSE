3ce2877b3f2fa90af76e6ec73770917fd2dd1fb0
fordream <27190664@qq.com>
2016-02-29 17:49:22 +0800

    Fix:

--- js/main.js
+++ js/main.js
@@ -6,51 +6,58 @@ function loginDisplay(){
 }
 
 /*the pictures switching starts*/
-var pSwitchPic = document.getElementsByClassName("pSwitchPic");
-var picsList = document.getElementsByClassName("picsList")[0];
-for(var i = 0;i < pSwitchPic.length;i++){
-    var li = document.createElement("li");
-    picsList.appendChild(li);
-}
-var lis = picsList.getElementsByTagName("li");
-var x = 0;
-function setLabel(){
-    pSwitchPic[x].setAttribute("class", "pSwitchPic active");
-    lis[x].setAttribute("class", "pointed");
-}
-function removeLabel(){
-    pSwitchPic[x].setAttribute("class","pSwitchPic");
-    lis[x].removeAttribute("class");
-}
-var picsPre = document.getElementsByClassName("picsPre");
-var picsNext = document.getElementsByClassName("picsNext");
-function addX(){
-    removeLabel();
-    x = x + 1;
-    if(x === pSwitchPic.length){
-        x = 0
+if(document.getElementsByClassName("pictures")) {
+    var pSwitchPic = document.getElementsByClassName("pSwitchPic");
+    var picsList = document.getElementsByClassName("picsList")[0];
+    for (var i = 0; i < pSwitchPic.length; i++) {
+        var li = document.createElement("li");
+        picsList.appendChild(li);
     }
-    setLabel();
-}
-function minusX(){
-    removeLabel();
-    x = x -1;
-    if(x === -1){
-        x = pSwitchPic.length - 1
+    var lis = picsList.getElementsByTagName("li");
+    var x = 0;
+
+    function setLabel() {
+        pSwitchPic[x].setAttribute("class", "pSwitchPic active");
+        lis[x].setAttribute("class", "pointed");
     }
-    setLabel();
-}
-picsNext.onclick = addX();
-picsPre.onclick = minusX();
-var liPoint = function(j){
-    lis[j].onclick = function(){
+
+    function removeLabel() {
+        pSwitchPic[x].setAttribute("class", "pSwitchPic");
+        lis[x].removeAttribute("class");
+    }
+
+    var picsPre = document.getElementsByClassName("picsPre");
+    var picsNext = document.getElementsByClassName("picsNext");
+
+    function addX() {
         removeLabel();
-        x = j;
+        x = x + 1;
+        if (x === pSwitchPic.length) {
+            x = 0
+        }
         setLabel();
     }
-}
-for(var j = 0;j < pSwitchPic.length;j ++){
-    liPoint(j)
+
+    function minusX() {
+        removeLabel();
+        x = x - 1;
+        if (x === -1) {
+            x = pSwitchPic.length - 1
+        }
+        setLabel();
+    }
+    picsNext.onclick = addX();
+    picsPre.onclick = minusX();
+    var liPoint = function (j) {
+        lis[j].onclick = function () {
+            removeLabel();
+            x = j;
+            setLabel();
+        }
+    };
+    for (var j = 0; j < pSwitchPic.length; j++) {
+        liPoint(j)
+    }
 }
 
 /*the pictures ends*/
